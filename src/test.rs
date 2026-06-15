690f593699b51ac2963a6b64b94690f650d73727
Nugraha <richiisei@gmail.com>
2026-06-15 07:14:55 +0700

    Add PacketProcessor unit tests (noop + header) to test.rs

--- src/test.rs
+++ src/test.rs
@@ -187,3 +187,72 @@ fn timeout() {
     assert_eq!(events.len(), 1);
     assert!(events[0].is_disconnect_and(|event| event.from == 1));
 }
+
+#[test]
+fn packet_processor_connect() {
+    use crate::{PacketProcessor, Box};
+    struct Noop;
+    impl PacketProcessor for Noop {
+        fn header_size(&self) -> usize { 0 }
+        fn write_outgoing(&mut self, _: &mut [u8], _: u16, _: u16) {}
+        fn validate_incoming(&mut self, _: &[u8], _: u16, _: u16) -> Option<u16> {
+            Some(0)
+        }
+    }
+
+    let mut network = Network::new();
+    let host1 = network.create_host(enet::HostSettings {
+        peer_limit: 1,
+        packet_processor: Some(Box::new(Noop)),
+        ..Default::default()
+    });
+    let host2 = network.create_host(enet::HostSettings {
+        peer_limit: 1,
+        packet_processor: Some(Box::new(Noop)),
+        ..Default::default()
+    });
+
+    network.connect(host1, host2, 255, 5);
+    network.update(1);
+    let events = network.update(1);
+    assert_eq!(events.len(), 2);
+    assert!(events[0].is_connect_and(|e| e.to == host1));
+    assert!(events[1].is_connect_and(|e| e.to == host2));
+}
+
+#[test]
+fn packet_processor_with_header() {
+    use crate::{PacketProcessor, Box};
+
+    /// 4-byte LE-u32 header with XOR trick: header[0..4] = port as LE u32.
+    struct XorHeader;
+    impl PacketProcessor for XorHeader {
+        fn header_size(&self) -> usize { 4 }
+        fn write_outgoing(&mut self, header: &mut [u8], _peer_id: u16, port: u16) {
+            header[0..2].copy_from_slice(&port.to_le_bytes());
+        }
+        fn validate_incoming(&mut self, header: &[u8], port: u16, _reserved: u16) -> Option<u16> {
+            let got = u16::from_le_bytes([header[0], header[1]]);
+            if got == port { Some(1) } else { None }
+        }
+    }
+
+    let mut network = Network::new();
+    let host1 = network.create_host(enet::HostSettings {
+        peer_limit: 1,
+        packet_processor: Some(Box::new(XorHeader)),
+        ..Default::default()
+    });
+    let host2 = network.create_host(enet::HostSettings {
+        peer_limit: 1,
+        packet_processor: Some(Box::new(XorHeader)),
+        ..Default::default()
+    });
+
+    network.connect(host1, host2, 255, 5);
+    network.update(1);
+    let events = network.update(1);
+    assert_eq!(events.len(), 2);
+    assert!(events[0].is_connect_and(|e| e.to == host1));
+    assert!(events[1].is_connect_and(|e| e.to == host2));
+}
